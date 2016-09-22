86cafda2be33000c181147b0d63ca78cdf2b897f
galenlin <oolgloo.2012@gmail.com>
2016-09-22 14:31:54 +0800

    Use Pbind registered resources bundles to resolve images

--- Pbind/Classes/View/UIImageView+Pbind.m
+++ Pbind/Classes/View/UIImageView+Pbind.m
@@ -8,15 +8,13 @@
 
 #import "UIImageView+Pbind.h"
 #import "UIView+Pbind.h"
+#import "Pbind+API.h"
 
 @implementation UIImageView (Pbind)
 
 - (void)setImageName:(NSString *)imageName {
     UIImage *image = nil;
-    // TODO: Add patch bundle
-    NSArray *preferredBundles = @[[NSBundle bundleForClass:self.supercontroller.class],
-                                 [NSBundle mainBundle]
-                                 /* Patch bundle */];
+    NSArray *preferredBundles = [Pbind allResourcesBundles];
     for (NSBundle *bundle in preferredBundles) {
         image = [UIImage imageNamed:imageName inBundle:bundle compatibleWithTraitCollection:nil];
         if (image != nil) {
