63f1092ce16dd6473b8f204292eb7b91899e1a5c
galenlin <oolgloo.2012@gmail.com>
2017-01-15 19:07:38 +0800

    Add comments for PBAction.h

--- Pbind/Classes/Action/PBAction.h
+++ Pbind/Classes/Action/PBAction.h
@@ -24,27 +24,66 @@
 
 @end
 
+/**
+ This class define the standard of interactions.
+ */
 @interface PBAction : NSObject<PBAction>
+
+#pragma mark - Register
+///=============================================================================
+/// @name Register
+///=============================================================================
 
 + (void)registerType:(NSString *)type;
 + (void)registerTypes:(NSArray *)types;
 + (PBAction *)actionForType:(NSString *)type;
 
+#pragma mark - Matching
+///=============================================================================
+/// @name Matching
+///=============================================================================
+
+/** The type for the action to be triggered */
 @property (nonatomic, strong) NSString *type;
+
+#pragma mark - Triggering
+///=============================================================================
+/// @name Triggering
+///=============================================================================
+
+/** The target of the action */
 @property (nonatomic, strong) id target;
+
+/** The name of the action */
 @property (nonatomic, strong) NSString *name;
+
+/** The parameters of the action */
 @property (nonatomic, strong) NSDictionary *params;
 
+/** Whether doesn't allow the action to be triggered, default is NO */
 @property (nonatomic, assign) BOOL disabled;
 
+/** The mappers to create the next actions */
 @property (nonatomic, strong) NSDictionary *nextMappers;
 
+/** The store of current action */
 @property (nonatomic, weak) PBActionStore *store;
 
+/**
+ Check if has next action with the specified key
+
+ @param key the key for the next actions
+ @return YES if got one
+ */
 - (BOOL)haveNext:(NSString *)key;
 - (void)dispatchNext:(NSString *)key;
 
 @end
+
+#pragma mark - Annotation
+///=============================================================================
+/// @name Annotation
+///=============================================================================
 
 #define pbaction(__type__) \
 synthesize type=_type; \
@@ -55,5 +94,5 @@ synthesize type=_type; \
 #define pbactions(...) \
 synthesize type=_type; \
 + (void)load { \
-[self registerTypes:@[__VA_ARGS__]]; \
+    [self registerTypes:@[__VA_ARGS__]]; \
 }
