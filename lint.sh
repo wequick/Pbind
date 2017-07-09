4a43bbd7e24ed33e9fafbbe0cbbd18ae8b82ad5b
galenlin <oolgloo.2012@gmail.com>
2017-07-09 16:58:25 +0800

    Update CI

--- lint.sh
+++ lint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-set -o pipefail && xcodebuild test -workspace Example/Pbind.xcworkspace -scheme Pbind-Example -destination 'platform=iOS Simulator,name=iPhone 5S,OS=10.2' | xcpretty
+set -o pipefail && xcodebuild test -workspace Example/Pbind.xcworkspace -scheme Pbind-Example -destination 'platform=iOS Simulator,name=iPhone 6S,OS=10.2' | xcpretty
 pod lib lint
