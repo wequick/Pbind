a4c50fa81d1ec74a65263ebb38d56cd53ae34dbc
galenlin <oolgloo.2012@gmail.com>
2017-09-24 23:32:14 +0800

    Add license header

--- Pbind/Classes/View/UIView+PBAction.h
+++ Pbind/Classes/View/UIView+PBAction.h
@@ -1,9 +1,12 @@
 //
 //  UIView+PBAction.h
-//  Pods
+//  Pbind <https://github.com/wequick/Pbind>
 //
 //  Created by galen on 17/8/14.
+//  Copyright (c) 2015-present, Wequick.net. All rights reserved.
 //
+//  This source code is licensed under the MIT-style license found in the
+//  LICENSE file in the root directory of this source tree.
 //
 
 #import <UIKit/UIKit.h>
